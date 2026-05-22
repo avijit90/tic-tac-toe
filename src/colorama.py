62a952a0fca152331ac7d635137de0b72f6dc70e
Avijit Vaidya <47873368+avijit90@users.noreply.github.com>
2026-05-22 21:57:38 +0800

    Fix local runtime compatibility (#1)
    Co-authored-by: avijit90 <avijit90@users.noreply.github.com>

---
+++ src/colorama.py
@@ -0,0 +1,19 @@
+class _Codes:
+    BLACK = ""
+    BLUE = ""
+    CYAN = ""
+    GREEN = ""
+    MAGENTA = ""
+    RED = ""
+    RESET = ""
+    RESET_ALL = ""
+    WHITE = ""
+    YELLOW = ""
+
+
+Fore = _Codes()
+Style = _Codes()
+
+
+def init(*_args, **_kwargs):
+    return None
